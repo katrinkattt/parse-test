392af5c4fd75f9a5858f62b370e057caab8a3fef
Nkitina Ekaterina <52584077+katrinnnKat@users.noreply.github.com>
2019-12-07 20:21:23 +0500

    not ready yet
    совсем сырой

--- main.py
+++ main.py
@@ -1,159 +1,49 @@
-import datetime
-import urllib.parse
-from collections import namedtuple
-
-import  bs4
 import requests
+from bs4 import BeautifulSoup
 
-InnerBlock = namedtuple('Block', 'title, price,currency,date,url')
+global str
+str = str(map)
 
-print('is work')
+r = requests.get('https://www.avito.ru/tyumen/transport')
+print(r.status_code)
 
-class Block(InnerBlock):
-    def __str__(self):
-        return f'{self.title}\t{self.price} {self.currency}\t{self.date}\t{self.url}'
 
-# x = Block(title='x', price='84', currency=111, date=222, url='x')
-# print(x)
+if r.status_code == 200:
+    print("it's ok")
+elif r.status_code == 404:
+    print('not found')
 
-class AvitoParser:
 
-    def __init__(self):
-        self.session = requests.Session()
-        self.session.headers = {
-            'UserAgent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:70.0) Gecko/20100101 Firefox/70.0',
-            'Accept-Language': 'ru',
-        }
-    def get_page(self, page: int = None):
-        params = {
-            'radius':200,
-            'cd':1
-        }
-        if page and page > 1:
-            params['p'] = page
+res = r.content
+e = r.encoding = 'utf-8'
+he = r.headers
+# text_res = r.text        #NO WORK
 
-        url = 'https://www.avito.ru/tyumen/avtomobili'
-        r = self.session.get(url, params=params)
-        return r.text
-    
-    @staticmethod
-    def parse_date(item: str):
-        params = item.strip().split(' ')
-        # print(params)
-        if len(params) == 2:
-            day, time = params
-            if day == 'Сегодня':
-                date = datetime.date.today()
-            elif day == 'Вчера':
-                date = datetime.date.today() - datetime.timedelta(days=1)
-            else:
-                print('Не смогли разобрать день:', item)
-                return
+print(res)
+print(e)
+print(he)
 
-            time = datetime.datetime.strptime(time, '%H:%M').time()
-            return datetime.datetime.combine(date=date, time=time)
+# print(text_res)
 
-        elif len(params) == 3:
-            day, month_hru, time = params
-            day = int(day)
-            months_map = {
-                'января': 1,
-                'февраля': 2,
-                'марта': 3,
-                'апреля': 4,
-                'мая': 5,
-                'июня': 6,
-                'июля': 7,
-                'августа': 8,
-                'сентября': 9,
-                'октября': 10,
-                'ноября': 11,
-                'декабря': 12,
-            }
-            month = months_map.get(month_hru)
-            if not month:
-                print('Не смогли разобрать месяц:', item)
-                return
 
-            today = datetime.datetime.today()
-            time = datetime.datetime.strptime(time, '%H:%M')
-            return datetime.datetime(day=day, month=month, year=today.year, hour=time.hour, minute=time.minute)
+# soup = BeautifulSoup('https://www.avito.ru/tyumen/transport')
+# s = soup.prettify()         #POCHTI WORK return tag:HTML, BODY, DIV
 
-        else:
-            print('Не смогли разобрать формат:', item)
-            return
+# soup = BeautifulSoup(res)         #POKA NE WORK TK LXML no supports
 
-    def parse_block(self, item):
-        url_block = item.select_one('a.item-description-title-link')
-        href = url_block.get('href')
-        if href:
-            url = 'https://www.avito.ru'+ href
-        else:
-            url = None
-        
-        title_block = item.select_one('h3.title.item-description-title span')
-        title = title_block.string.strip()
+# https:api.github.com
 
-        price_block = item.select_one('span.price')
-        price_block = price_block.get_text('\n')
-        price_block = list(filter(None, map(lambda i: i.strip(), price_block.split('\n'))))
-        if len(price_block) == 2:
-            price, currency = price_block
-        elif len(price_block) == 1:
-            price,  = 0, None
-        else:
-            price, currency = None, None
-            print(f'Что-то пошло не так при поиске цены: {price_block}, {url}')
-        
-        date = None
-        date_block = item.select_one('div.item-date div.js-item-date.c-2')
-        absolute_date = date_block.get('data-absolute-date')
-        if absolute_date:
-            date = self.parse_date(item=absolute_date)
+# DA BLYAT' POCHEMU WORK TOL'KO S ETIM SAITOM-.-
+# url = 'https://www.icc-ccs.org/index.php'
 
-        return Block(
-            url = url,
-            title = title,
-            price = price,
-            currency = currency,
-            date = date,
-        )
+# page = requests.get(url)
+# print(page.status_code)
+# print(page.text)
 
-    def get_pagination_limit(self):
-        text = self.get_page()
-        soup = bs4.BeautifulSoup(text, 'lxml')
+# soup = BeautifulSoup(page.text, 'html.parser')
 
-        container = soup.select('a.pagination-page')
-        last_button = container[-1]
-        href = last_button.get('href')
-        if not href:
-            return 1
+# print(soup.prettify())
 
-        r = urllib.parse.urlparse(href)
-        params = urllib.parse.parse_qs(r.query)
-        return int(params['p'][0])
 
-    def get_blocks(self, page: int = None):
-        text = self.get_page(page=page)
-        soup = bs4.BeautifulSoup(text, 'lxml')
-
-        container = soup.select('div.item.item_table.clearfix.js-catalog-item-enum.item-with-contact.js-item-extended')
-        for item in container:
-
-            block = self.parse_block(item=item)
-            print(block)
-
-    def parse_all(self):
-        limit = 3
-        print(f'')
-        for i in range(1, limit + 1):
-            self.get_blocks(page=i)
-
-def main():
-    p = AvitoParser()
-    p.parse_all()
-
-if __name__ == "__main__":
-    main()
 
 
